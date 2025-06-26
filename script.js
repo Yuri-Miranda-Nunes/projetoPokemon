ce06b899c715d4b617b7bfc4eb16a91f33fb6559
Yuri-Miranda-Nunes <yurimirandanunes1@gmail.com>
2025-06-26 09:02:21 -0300

    Atualiza imagem do Pikachu e adiciona funcionalidades para trocar Pokémon

---
+++ script.js
@@ -0,0 +1,10 @@
+function trocarcharmander(){
+    document.getElementById("pokemon-name").innerHTML = "Charmander";
+    document.getElementById("pokemon-img").src = "https://static.wikia.nocookie.net/pokemonet/images/8/87/004CharmanderFRLG.png/revision/latest?cb=20130505000902&path-prefix=pt-br";
+}
+function trocarSquirtle(){
+    document.getElementById("pokemon-name").innerHTML = "Squirtle";
+}
+function Home() {
+    window.location.href = 'index.html';
+}
