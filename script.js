ad9b961f4f4c9e3732c256a27ea2219e7a18ce2c
Yuri-Miranda-Nunes <yurimirandanunes1@gmail.com>
2025-07-21 09:35:41 -0300

    Adiciona informações e ataques para Charmander e Squirtle

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function trocarcharmander(){
-    document.getElementById("pokemon-name").innerHTML = "Charmander";
-    document.getElementById("pokemon-img").src = "https://static.wikia.nocookie.net/pokemonet/images/8/87/004CharmanderFRLG.png/revision/latest?cb=20130505000902&path-prefix=pt-br";
+    document.getElementById("pokemon-name").innerText = "Charmander";
+    document.getElementById("pokemon-img").src = "https://img.pokemondb.net/artwork/large/charmander.jpg";
+    document.getElementById("pokemon-type").innerText = "Tipo: Fogo";
+    document.getElementById("attacks-list").innerHTML = "<li>Chama</li><li>Arranhão</li><li>Golpe de Cauda</li>";
 }
+
 function trocarSquirtle(){
     document.getElementById("pokemon-name").innerHTML = "Squirtle";
+    document.getElementById("pokemon-img").src = "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/007.png";
+    document.getElementById("pokemon-type").innerHTML = "Tipo: Água";
+    document.getElementById("attacks-list").innerHTML = "<li>Jato d'Água</li><li>Investida</li><li>Giro Rápido</li>";
 }
 function Home() {
     window.location.href = 'index.html';
